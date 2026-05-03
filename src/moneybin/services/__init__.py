c218e47a3ad33148bf1a2bb040d639acf9dbf07d
Brandon Saffel <bsaffel@gmail.com>
2026-05-03 11:07:28 -0400

    Export AccountService from services package init

--- src/moneybin/services/__init__.py
+++ src/moneybin/services/__init__.py
@@ -1,5 +1,6 @@
 """Service layer for MoneyBin business logic."""
 
+from moneybin.services.account_service import AccountService
 from moneybin.services.matching_service import MatchingService
 
-__all__ = ["MatchingService"]
+__all__ = ["AccountService", "MatchingService"]
